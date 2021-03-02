89e5fbf9ce2bb899c8ea91a51a9c6cde161106f4
babs20 <thevillemc@gmail.com>
2021-03-02 12:58:10 -0500

    fix syntax issue with item_price and total_price when displaying the orders#show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,11 @@
 class OrdersController < ApplicationController
-
   def show
     @order = Order.find(params[:id])
   end
 
   def create
     charge = perform_stripe_charge
-    order  = create_order(charge)
+    order = create_order(charge)
 
     if order.valid?
       empty_cart!
@@ -14,7 +13,6 @@ class OrdersController < ApplicationController
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
-
   rescue Stripe::CardError => e
     redirect_to cart_path, flash: { error: e.message }
   end
@@ -28,19 +26,20 @@ class OrdersController < ApplicationController
 
   def perform_stripe_charge
     Stripe::Charge.create(
-      source:      params[:stripeToken],
-      amount:      cart_subtotal_cents,
+      source: params[:stripeToken],
+      amount: cart_subtotal_cents,
       description: "Khurram Virani's Jungle Order",
-      currency:    'cad'
+      currency: 'cad',
     )
   end
 
   def create_order(stripe_charge)
-    order = Order.new(
-      email: params[:stripeEmail],
-      total_cents: cart_subtotal_cents,
-      stripe_charge_id: stripe_charge.id, # returned by stripe
-    )
+    order =
+      Order.new(
+        email: params[:stripeEmail],
+        total_cents: cart_subtotal_cents,
+        stripe_charge_id: stripe_charge.id, # returned by stripe
+      )
 
     enhanced_cart.each do |entry|
       product = entry[:product]
@@ -48,12 +47,11 @@ class OrdersController < ApplicationController
       order.line_items.new(
         product: product,
         quantity: quantity,
-        item_price: humanized_money_with_symbol product.price,
-        total_price: humanized_money_with_symbol product.price * quantity
+        item_price: product.price,
+        total_price: product.price * quantity,
       )
     end
     order.save!
     order
   end
-
 end
