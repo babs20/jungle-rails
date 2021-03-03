2173aabec811f5968e1dd5c983cda61db4d1915c
babs20 <thevillemc@gmail.com>
2021-03-03 14:46:12 -0500

    update route on authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to %i[sessions new] unless current_user
+    redirect_to '/login' unless current_user
   end
 end
