62132e81ff91089be45904b842e35da3a997b2e0
babs20 <thevillemc@gmail.com>
2021-03-08 16:45:20 -0500

    add .authenticate_with _credentials and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, length: { minimum: 8 }
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    formatted_email = email.strip.downcase
+
+    user = User.where('LOWER(email) = ?', [formatted_email])[0]
+    user && user.authenticate(password) ? user : nil
+  end
 end
