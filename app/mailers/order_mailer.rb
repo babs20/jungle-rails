7689359c7d77e9fadd7b4877558662c85d5bc35a
babs20 <thevillemc@gmail.com>
2021-03-09 14:38:06 -0500

    change name to order_email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,9 @@
 class OrderMailer < ApplicationMailer
   default from: ENV['GMAIL_USERNAME']
 
-  def welcome_email(order)
+  def order_email(products, order)
     @order = order
+    @products = products
     @url = 'http://www.jungle.com'
     mail(to: order.email, subject: "Your jungle.com order ##{order.id}")
   end
