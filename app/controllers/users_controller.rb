5f8af96c786b47e590081dd288bae48bf98602ca
babs20 <thevillemc@gmail.com>
2021-03-09 21:41:00 -0500

    add error message for incorrect register info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
-  def new; end
-  def create
-    user = User.new(user_params)
+  def new
+    @user = User.new
+  end
 
-    if user.save
-      session[:user_id] = user.id
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to :root
     else
-      redirect_to '/signup'
+      render :new
     end
   end
 
